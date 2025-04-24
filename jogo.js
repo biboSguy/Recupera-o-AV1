dc814f16c33b381e173e7894498871be9e54ce55
ENZO CASSOL DE DÉA <enzo_dea@estudante.sesisenai.org.br>
2025-04-24 07:50:46 -0300

    novos metodos

--- jogo.js
+++ jogo.js
@@ -8,15 +8,21 @@ class Personagem{
 class Guerreiro extends Personagem{
     constructor(vida, força, mana){
         super(vida, força, mana)
+        this.ataca = ataca
+        this.defende = defende
     }
 }
 class Mago extends Personagem{
     constructor(vida, força, mana){
         super(vida, força, mana)
+        this.magia = magia
+        this.desvia = desvia
     }
 }
 class Arquiteto extends Personagem{
     constructor(vida, força, mana){
         super(vida, força, mana)
+        this.projeta = projeta
+        this.construir = construir
     }
 }
