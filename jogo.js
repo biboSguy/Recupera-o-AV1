9e2a6273c186862d887a73eee9b9b08146d43eeb
ENZO CASSOL DE DÉA <enzo_dea@estudante.sesisenai.org.br>
2025-04-24 08:00:14 -0300

    Aplicação de polimorfismo no método atacar

--- jogo.js
+++ jogo.js
@@ -11,6 +11,16 @@ class Guerreiro extends Personagem{
         this.ataca = ataca
         this.defende = defende
     }
+    ataca(){
+        this.ataca = true 
+        this.mana = -5
+        console.log("guerreiro atacou")
+    }
+    defende(){
+        this.defende = true
+        this.mana = -10
+        console.log("guerreiro defendeu")
+    }
 }
 class Mago extends Personagem{
     constructor(vida, força, mana){
@@ -18,11 +28,31 @@ class Mago extends Personagem{
         this.magia = magia
         this.desvia = desvia
     }
+    magia(){
+        this1.magia = true
+        this.mana = -15
+        console.log("mago fez uma magia")
+    }
+    desvia(){
+        this.desvia = true
+        this.mana = -5
+        console.log("mago desviou")
+    }
 }
 class Arquiteto extends Personagem{
     constructor(vida, força, mana){
         super(vida, força, mana)
-        this.projeta = projeta
-        this.construir = construir
+        this.ataca = ataca
+        this.defende = defende
+    }
+    ataca(){
+        this.ataca = true
+        this.mana = -10
+        console.log("arquiteto atacou")
+    }
+    defende(){
+        this.defende = true
+        this.mana = -15
+        console.log("arquiteto defendeu")
     }
 }
